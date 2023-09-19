182febc8a6b575ae743f4835d5ed084060f93472
bo-nicolaisen <bni@techcollege.dk>
2023-09-19 07:56:13 +0200

    initial commit
    data

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,16 @@
-function
+
+// 10 hurs
+const timeArray = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00'];
+const temperatureArray = [22, 24, 23, 25, 26, 27, 21, 20, 29, 28];
+const windSpeedArray = [15, 16, 17, 13, 14, 12, 18, 11, 10, 19];
+const windDirectionArray = [226, 221, 229, 227, 224, 225, 230, 223, 219, 228];
+const weatherCodes = [81, 82, 83, 82, 80, 82, 81, 83, 80, 83];
+
+// days
+const weekDays = ['Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag', 'Søndag'];
+const dayTemperatureArray = [22, 24, 23, 25, 26, 27, 21];
+const nightTemperatureArray = [12, 11, 10, 8, 6, 7, 9];
+const averageWindSpeedArray = [15, 16, 17, 13, 14, 12, 18];
+const averageWindDirectionArray = [226, 221, 229, 227, 224, 225, 230];
+const dayWeatherCodes = [81, 82, 83, 82, 80, 82, 81];
+
